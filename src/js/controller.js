93f07b0761b15b9c0054e1404b6c1b3059eca4a5
Jitendra Gupta <jiitendra.gupta@outlook.com>
2026-03-08 11:35:03 +0530

    Fix icons

--- src/js/controller.js
+++ src/js/controller.js
@@ -9,7 +9,7 @@ import bookmarksView from './views/bookmarksView.js';
 import addRecipeView from './views/addRecipeView.js';
 
 // Import SVG sprite so Parcel bundles it
-import icons from 'url:../img/icons.svg';
+import icons from 'url:src/img/icons.svg';
 
 // Fix SVG icon references for production
 const fixSVGIcons = () => {
